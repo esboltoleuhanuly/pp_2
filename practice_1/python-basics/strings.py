4a749ac88bfb0958dd48c47c0907317696a9a6f6
Esbol Toleuhanuly <esboltoleuhanuly@gmail.com>
2026-01-24 14:00:15 +0500

    new commit

---
+++ practice_1/python-basics/strings.py
@@ -0,0 +1,23 @@
+#1
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#2
+a = "Hello"
+print(a)
+
+#3
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+#4
+for x in "banana":
+  print(x)
+
+#5
+a = "Hello, World!"
+print(len(a))
