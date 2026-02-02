a21c4f619edcf94d5ffa99b51e5c28881f0ae052
Esbol Toleuhanuly <esboltoleuhanuly@gmail.com>
2026-02-02 14:44:00 +0500

    stop

---
+++ practices/Practice_1/python-basics/syntax.py
@@ -0,0 +1,20 @@
+#1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#3
+print("Python is fun!")
+
+#4
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+#5
+print("Hello"); print("How are you?"); print("Bye bye!")
